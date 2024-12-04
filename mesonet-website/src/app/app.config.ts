52ce3f519930c11faddd1b859e6123b0f4e84df3
cherryleh <cherryle@hawaii.edu>
2024-12-04 13:17:39 -1000

    Adjust routing for hash

--- mesonet-website/src/app/app.config.ts
+++ mesonet-website/src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { appRoutes } from './app.routes';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation())]
--- mesonet-website/src/app/app.routes.ts
+++ mesonet-website/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-export const appRoutes: Routes = [
+export const routes: Routes = [
     { path: '', component: HomeComponent }, // Default route
     { path: 'about', component: AboutComponent }, // About page route
     { path: 'data-display', component: DataDisplayComponent }, // Data display route
