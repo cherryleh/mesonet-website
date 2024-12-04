7a32874c58ce1e0152c70b7af945f71876b33630
cherryleh <cherryle@hawaii.edu>
2024-12-04 13:20:56 -1000

    Update main.ts

--- mesonet-website/src/main.ts
+++ mesonet-website/src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app.routes';
+import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(routes),
     provideHttpClient() // Provide HttpClient
   ]
 }).catch(err => console.error(err));
