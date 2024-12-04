a3c923a273f687d9bdb4a75c509c366ea140c1dd
cherryleh <cherryle@hawaii.edu>
2024-12-03 15:58:34 -1000

    Created NotFound Component

--- mesonet-website/src/app/app.routes.ts
+++ mesonet-website/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
+import { NotFoundComponent } from './not-found/not-found.component'; 
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent }, // Default route
     { path: 'about', component: AboutComponent }, // About page route
-    {path: 'data-display', component: DataDisplayComponent},
-    { path: '**', redirectTo: '', pathMatch: 'full' } // Fallback route
-  ];
+    { path: 'data-display', component: DataDisplayComponent }, // Data display route
+    { path: '**', component: NotFoundComponent } // Fallback route
+];
