1e16468d6bc84fa82321b8b339b685078032cd33
cherryleh <cherryle@hawaii.edu>
2024-12-03 16:44:11 -1000

    Update app.component.ts

--- mesonet-website/src/app/app.component.ts
+++ mesonet-website/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 // import { DataDisplayComponent } from './data-display/data-display.component';
 
 
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, RouterOutlet],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
